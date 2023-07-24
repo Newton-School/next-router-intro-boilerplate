3253fdd25a577a95954899edb24c2e49d1955ea0
gautamsarawagi <gautamsarawagi1@gmail.com>
2023-07-24 16:25:28 +0530

    Boilerplate Changes

--- app/info/page.js
+++ app/info/page.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
-function Info() {
-  return (
-    <div>
-      {/*Info Page Content*/}
-    </div>
-  )
+const Info = () => {
+
+
+    return (
+        <div id="info-page">
+
+
+            <h1> Info page </h1>
+            <h2>I am born in India</h2>
+            <h2>and I like to play Cricket </h2>
+             <button id="info-to-blog"> Go to Blog</button>
+            <button id="info-to-home"> Go to Home</button>
+        </div>
+    )
 }
-
 export default Info
